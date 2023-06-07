882162362e286fd0679f7356d40f1769951dd02c
matias ignacio torres torres <matias.torres1812@gmail.com>
2023-06-07 18:00:20 -0400

    segundo commt

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import '../components/footer.css'
+
+function Footer(){
+    return (
+        <footer >
+            <p>Echo con amor por los alumnos de Desafio Latam </p> 
+        </footer>
+    )
+    }
+    export default Footer;
