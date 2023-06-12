e4fb56992b059e9d81d086becc7bd6112db50817
Fabián Peña <fabianpenatorres88@gmail.com>
2023-06-12 16:56:31 -0400

    home

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
-import '../views/home.css'
 
-function Home(){
-    return (
-      <>
-        <div className="boar">
-        <NavLink to="/joblist" className="text-base font-medium text-gray-500 dark:text-white hover:text-gray-900">
+
+export default function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <NavLink to="/joblist" className="text-base font-medium text-gray-500 dark:text-white hover:text-gray-900">
     Joblist
   </NavLink>
   <br/>
   <NavLink to="/login" className="text-base font-medium text-gray-500 dark:text-white hover:text-gray-900">
     Login
   </NavLink>
-            <div className="home">
-                <p className='parrafo'>Reinsercion Juvenil</p>
-                <h2 >Postula por un mejor mañana registrate <br/> y aca las empresas te contactan</h2>
-                <p >Somos una pagina web del sename que te proporcionara una variada cantidad de empleos <br className='espacio'/> en todo Chile en la cual solo debes dejarnos tus mejores habilidades <br /> y postular a tu empleo soñado, que esperas registrate ahora.</p>
-                <Button className='boton' variant="outline-secondary">Registrate aqui</Button>
-            </div>
-        </div>
-        </>
-    )
-    }
-    export default Home;
+  </>
+  )
+}
